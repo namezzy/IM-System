60801985713e5f01dbcc8a7d74dfc0c389c343bb
withLevi <zengyin.ssr@gmail.com>
2024-11-29 10:47:46 +0800

    Basic server build and compile.

--- server.go
+++ server.go
@@ -1,1 +1,52 @@
 package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type Server struct {
+	Ip   string
+	Port int
+}
+
+// Create a server interface
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:   ip,
+		Port: port,
+	}
+	return server
+}
+
+func (this *Server) Handler(conn net.Conn) {
+	// ...Currently connected businesses
+	fmt.Println("Connection established successfully. ")
+}
+
+// Start server interface
+func (this *Server) Start() {
+	// socket listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
+	if err != nil {
+		fmt.Println("net.Listen err:", err)
+		return
+	}
+
+	// close listen socket
+	defer listener.Close()
+
+	for {
+		// accept
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener accept err: ", err)
+			continue
+		}
+
+		// do handler
+		go this.Handler(conn)
+
+	}
+
+}
